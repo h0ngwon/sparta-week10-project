fe57b60c5f064f2fab1e1ee66b8ed93841401066
h0ngwon <w___on@naver.com>
2023-11-29 17:35:42 +0900

    Chore: 라우터 세팅

---
+++ src/components/ui/Nav.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+const Container = styled.div`
+	width: 100%;
+	height: 80px;
+	background-color: black;
+	color: white;
+	display: flex;
+	align-items: center;
+	justify-content: space-around;
+`;
+const LogoContainer = styled.div`
+	font-size: 36px;
+	cursor: pointer;
+`;
+
+const Menu = styled.ul`
+	display: flex;
+	gap: 20px;
+`;
+
+const MenuItem = styled.li`
+	cursor: pointer;
+	padding: 20px;
+	border-radius: 12px;
+
+	&:hover {
+		background-color: #ccc;
+	}
+`;
+
+const Nav = () => {
+	const navigate = useNavigate();
+
+	return (
+		<Container>
+			<LogoContainer
+				onClick={() => {
+					navigate('/');
+				}}
+			>
+				HOME
+			</LogoContainer>
+			<Menu>
+				<MenuItem
+					onClick={() => {
+						navigate('/profile');
+					}}
+				>
+					내 프로필
+				</MenuItem>
+				<MenuItem>로그아웃</MenuItem>
+			</Menu>
+		</Container>
+	);
+};
+
+export default Nav;
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,3 +1,7 @@
+import Login from 'components/Login';
+import Profile from 'components/Profile';
+import Register from 'components/Register';
+import Layout from 'components/ui/Layout';
 import Detail from 'pages/Detail';
 import Home from 'pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
@@ -6,8 +10,14 @@ const Router = () => {
 	return (
 		<BrowserRouter>
 			<Routes>
-				<Route path='/' element={<Home />} />
-				<Route path='/detail/:id' element={<Detail />} />
+				<Route element={<Layout />}>
+					<Route path='/' element={<Home />} />
+					<Route path='/detail/:id' element={<Detail />} />
+					<Route path='/profile' element={<Profile />} />
+				</Route>
+                
+				<Route path='/login' element={<Login />} />
+				<Route path='/register' element={<Register />} />
 			</Routes>
 		</BrowserRouter>
 	);
